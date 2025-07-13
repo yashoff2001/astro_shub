d457f24e03463e831af0586fedc104dc94bdaefa
yashoff2001 <yashnaglaoff@gmail.com>
2025-07-13 20:04:30 +0530

    Updated Routes

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,10 +1,7 @@
 import React from 'react'
-import MainSection from '../components/main-section'
 import AboutSection from '../components/about-section'
 import Services from '../components/services-section'
 import Testimonials from '../components/testimonials'
-import Destiny from '../components/destiny'
-import Footer from '../components/footer'
 // import Stars from './assets/stars'
 
 function AboutPage() {
